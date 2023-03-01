9d4ee367ab5559a364473e4048dba5820ee0b488
ShujinOfAhri <duongminhquang11357@gmail.com>
2023-03-01 21:00:14 +0700

    update

---
+++ Bài 1/script.js
@@ -0,0 +1,17 @@
+function isPrime(n) {
+    if (n <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  
+  function calculateSum() {
+    const a = parseInt(document.getElementById("a").value);
+    const b = parseInt(document.getElementById("b").value);
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+      if (isPrime(i)) sum += i;
+    }
+    document.getElementById("result").innerHTML = `Tổng các số nguyên tố từ ${a} đến ${b} là: ${sum}`;
+  }
